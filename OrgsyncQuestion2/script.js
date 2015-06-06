bfffb573797cb980147269bd2a2c9bf63b0ddf85
AJ <ajparadowski@gmail.com>
2015-06-06 15:25:18 -0500

    Began DB Call
    started call to post data to database

--- OrgsyncQuestion2/script.js
+++ OrgsyncQuestion2/script.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
-				$("#submitBtn").click(function(){
-					var firstName = $.trim($('#nameInput').val().split(',')[1]);
-					var lastName = $.trim($('#nameInput').val().split(',')[0]);
-					var status = $('#statusInput').val();
-					passInData(firstName,lastName,status);
-				});
+	$("#submitBtn").click(function() {
+		var firstName = $.trim($('#nameInput').val().split(',')[1]);
+		var lastName = $.trim($('#nameInput').val().split(',')[0]);
+		var status = $('#statusInput').val();
+		passInData(firstName, lastName, status);
+	});
 });
 
-function passInData(firstN, lastN ,status)
-{
-	$("#personTable")
-    .prepend($('<tr>')
-        .append($('<td>')
-            .append(lastN + ", " + firstN)
-        )
-        .append($('<td>')
-            .append(status)
-        )
-    );
+function passInData(firstN, lastN, status) {
+	var xmlhttp;
 
-	//$('#personTable').html("<tr>name </tr>,<tr>status</tr>");
-	//alert(name + ", " + status);
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST", "/member/save", false);
+	xmlhttp.send("lastN&firstN&status");
+	
+	
+	$("#personTable").prepend($('<tr>').append($('<td>').append(lastN + ", " + firstN)).append($('<td>').append(status)));
+
+	$("table").tablesorter();
+	var sorting = [[0, 0]];
+	$("table").trigger("update");
+	$("table").trigger("sorton", [sorting]);
+	return false;
 }
